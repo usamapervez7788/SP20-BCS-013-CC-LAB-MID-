eb3eaf21536bd6d75ac0ec4863e67c7a8209f246
usamapervez7788 <130973253+usamapervez7788@users.noreply.github.com>
2023-10-26 15:37:28 +0500

    Add files via upload

---
+++ CC_LabMid_Question3/CC_LabMid_Question3/Question3.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CC_LabMid_Question3
+{
+    partial class Question3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GeneratePasswordButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GeneratedPasswordLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GeneratePasswordButton
+            // 
+            this.GeneratePasswordButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GeneratePasswordButton.Location = new System.Drawing.Point(146, 196);
+            this.GeneratePasswordButton.Margin = new System.Windows.Forms.Padding(2);
+            this.GeneratePasswordButton.Name = "GeneratePasswordButton";
+            this.GeneratePasswordButton.Size = new System.Drawing.Size(108, 46);
+            this.GeneratePasswordButton.TabIndex = 0;
+            this.GeneratePasswordButton.Text = "Generate";
+            this.GeneratePasswordButton.UseVisualStyleBackColor = true;
+            this.GeneratePasswordButton.Click += new System.EventHandler(this.GeneratePasswordButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 134);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password Generator";
+            // 
+            // GeneratedPasswordLabel
+            // 
+            this.GeneratedPasswordLabel.AutoSize = true;
+            this.GeneratedPasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GeneratedPasswordLabel.ForeColor = System.Drawing.Color.ForestGreen;
+            this.GeneratedPasswordLabel.Location = new System.Drawing.Point(95, 251);
+            this.GeneratedPasswordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.GeneratedPasswordLabel.Name = "GeneratedPasswordLabel";
+            this.GeneratedPasswordLabel.Size = new System.Drawing.Size(0, 26);
+            this.GeneratedPasswordLabel.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(332, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Usama Pervez( SP20-BCS-013 )";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Question3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(422, 352);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.GeneratedPasswordLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GeneratePasswordButton);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Question3";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GeneratePasswordButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label GeneratedPasswordLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CC_LabMid_Question3/CC_LabMid_Question3/Question3.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CC_LabMid_Question3
+{
+    public partial class Question3 : Form
+    {
+        public Question3()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePasswordButton_Click(object sender, EventArgs e)
+        {
+            string firstName = "Usama";
+            string lastName = "Pervez";
+            string regNumber = "13";
+
+            if (firstName.Length < 1 || lastName.Length < 1 || regNumber.Length < 1)
+            {
+                MessageBox.Show("Please enter your first name, last name, and registration number.");
+                return;
+            }
+
+            // Create a StringBuilder to build the password
+            StringBuilder password = new StringBuilder();
+
+            // Add initials of first and last name
+            password.Append(firstName[0]);
+            password.Append(lastName[0]);
+
+            // Add registration number
+            password.Append(regNumber);
+
+            // Generate random uppercase alphabet
+            Random random = new Random();
+            password.Append((char)random.Next('A', 'Z' + 1));
+
+            // Generate 3 random numbers (1 less than previously)
+            for (int i = 0; i < 3; i++)
+            {
+                password.Append((char)random.Next('0', '9' + 1));
+            }
+
+            // Generate 2 special characters (same as previously)
+            string specialCharacters = "!@#$%^&*()_-+=<>?";
+            for (int i = 0; i < 2; i++)
+            {
+                password.Append(specialCharacters[random.Next(specialCharacters.Length)]);
+            }
+
+            // Shuffle the password characters for better security
+            password = ShuffleString(password);
+
+            // Limit the password to a maximum length of 16 (1 less than previously)
+            if (password.Length > 15)
+            {
+                password.Length = 15;
+            }
+
+            // Display the generated password
+            GeneratedPasswordLabel.Text = password.ToString();
+        }
+
+        private StringBuilder ShuffleString(StringBuilder str)
+        {
+            Random random = new Random();
+            int n = str.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                char value = str[k];
+                str[k] = str[n];
+                str[n] = value;
+            }
+            return str;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
