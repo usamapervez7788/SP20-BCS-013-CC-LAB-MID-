c23b56237bbbe173938b990f35008c6afa49e8c8
usamapervez7788 <130973253+usamapervez7788@users.noreply.github.com>
2023-10-26 15:43:16 +0500

    Add files via upload

---
+++ qs 2 cc mid lab/LL1_MID_Task2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace LL1_MID_Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ParseButton = new System.Windows.Forms.Button();
+            this.InputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ParseButton
+            // 
+            this.ParseButton.Location = new System.Drawing.Point(111, 127);
+            this.ParseButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ParseButton.Name = "ParseButton";
+            this.ParseButton.Size = new System.Drawing.Size(93, 24);
+            this.ParseButton.TabIndex = 0;
+            this.ParseButton.Text = "Parse";
+            this.ParseButton.UseVisualStyleBackColor = true;
+            this.ParseButton.Click += new System.EventHandler(this.ParseButton_Click);
+            // 
+            // InputRichTextBox
+            // 
+            this.InputRichTextBox.Location = new System.Drawing.Point(77, 37);
+            this.InputRichTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.InputRichTextBox.Name = "InputRichTextBox";
+            this.InputRichTextBox.Size = new System.Drawing.Size(163, 64);
+            this.InputRichTextBox.TabIndex = 1;
+            this.InputRichTextBox.Text = "";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(138, 168);
+            this.ResultLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(35, 13);
+            this.ResultLabel.TabIndex = 2;
+            this.ResultLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(345, 207);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.InputRichTextBox);
+            this.Controls.Add(this.ParseButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ParseButton;
+        private System.Windows.Forms.RichTextBox InputRichTextBox;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ qs 2 cc mid lab/LL1_MID_Task2/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LL1_MID_Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ParseButton_Click(object sender, EventArgs e)
+        {
+            string input = InputRichTextBox.Text;
+            Parser parser = new Parser(input);
+
+            if (parser.ParseList())
+            {
+                ResultLabel.Text = "Valid input!";
+            }
+            else
+            {
+                ResultLabel.Text = "Invalid input!";
+            }
+        }
+        public class Parser
+        {
+            private string[] tokens;
+            private int index;
+
+            public Parser(string input)
+            {
+                this.tokens = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                this.index = 0;
+            }
+
+            public bool ParseList()
+            {
+                if (ParseItem())
+                {
+                    return ParseRest();
+                }
+                return false;
+            }
+
+            private bool ParseRest()
+            {
+                if (index < tokens.Length && tokens[index] == ",")
+                {
+                    index++;
+                    if (ParseItem())
+                    {
+                        return ParseRest();
+                    }
+                    return false;
+                }
+                return true; // ε-production
+            }
+
+            private bool ParseItem()
+            {
+                if (index < tokens.Length)
+                {
+                    string token = tokens[index];
+                    if (token == "id" || token == "num" || token == "string")
+                    {
+                        index++;
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+
+}
